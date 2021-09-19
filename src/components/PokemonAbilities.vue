e9c12443caf42e8f6d80afabc3bcd76bc0a6e903
phronesys <daniel.dazarola@gmail.com>
2021-09-19 04:47:01 -0300

    enhancements(abilities): design

--- src/components/PokemonAbilities.vue
+++ src/components/PokemonAbilities.vue
@@ -17,7 +17,7 @@
   @apply bg-white h-[200px] w-full;
 }
 .abilities .nes-container ul {
-  @apply flex flex-row flex-wrap gap-x-16 gap-y-8 ml-16;
+  @apply flex flex-row flex-wrap gap-x-16 gap-y-4 ml-4 py-4 h-full overflow-y-auto;
 }
 .abilities .nes-container h1 {
   @apply space-x-4 select-none;
