51b5f307497bc77003edbaf012f68b31ec90066d
phronesys <daniel.dazarola@gmail.com>
2021-09-19 04:46:27 -0300

    feat(search): filter pokemon list

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -1,12 +1,27 @@
 <template>
-  <div
-    class="bg-red-600 nes-field nes-container is-rounded is-inline text-white"
-  >
+  <div class="search nes-field nes-container is-rounded is-inline">
     <label for="name">Search</label>
-    <input type="text" id="name" class="nes-input bg-red-200 text-gray-800" />
+    <input
+      v-model="entry"
+      @keyup="$emit('filter', entry)"
+      type="text"
+      placeholder="Pokémon"
+      id="name"
+      class="nes-input"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+let entry = ref("");
+</script>
 
-<style></style>
+<style lang="postcss" scoped>
+input {
+  @apply bg-red-200 text-gray-800;
+}
+.search {
+  @apply bg-red-600 text-white;
+}
+</style>
